5646faa92ba0d671f3cd064a27f13b6bb8f396b7
olympiawoj <owojcik7@gmail.com>
2020-07-20 13:51:48 -0400

    Word Match & Update Score

---
+++ script.js
@@ -0,0 +1,74 @@
+const word = document.getElementById('word');
+const text = document.getElementById('text');
+const scoreEl = document.getElementById('score');
+const timeEl = document.getElementById('time');
+const endGameEl = document.getElementById('end-game')
+const settingsBtn = document.getElementById('settings-btn')
+const settings = document.getElementById('settings')
+const settingsForm = document.getElementById('settings-forms')
+const difficultySelect = document.getElementById('difficulty')
+
+// List of words for game
+// List of words for game
+const words = [
+    'sigh',
+    'tense',
+    'airplane',
+    'ball',
+    'pies',
+    'juice',
+    'warlike',
+    'bad',
+    'north',
+    'dependent',
+    'steer',
+    'silver',
+    'highfalutin',
+    'superficial',
+    'quince',
+    'eight',
+    'feeble',
+    'admit',
+    'drag',
+    'loving'
+];
+
+// Init word
+let randomWord;
+
+// Init score
+let score = 0;
+
+// Init time
+let time = 10;
+
+// Generate random word from array
+function getRandomWord(){
+    return words[Math.floor(Math.random() * words.length)]
+}
+
+// Add word to DOM
+function addWordToDOM(){
+    randomWord = getRandomWord();
+    word.innerHTML = randomWord;
+}
+
+// Update Score
+function updateScore(){
+    score++;
+    scoreEl.innerHTML = score;
+}
+
+addWordToDOM()
+
+// Event Listeners
+text.addEventListener('input',  e => {
+    const insertedText = e.target.value
+    if(insertedText === randomWord){
+        // change word
+        addWordToDOM()
+        updateScore()
+        // Clear 
+        e.target.value = ''
+    }
+})
